af6760b40d4e5b121b464fd055e53ee71b80b88b
dt8 <dt8@game4219.com>
2021-07-13 17:11:01 +0800

    Example task input modified.

--- dg_ai_platform/example.py
+++ dg_ai_platform/example.py
@@ -1,7 +1,18 @@
 from dg_ai_platform.dg_platform import ITaskProcess
-from PIL import Image
+from PIL import Image, ImageOps
 
 class ExampleTask(ITaskProcess):
+    def __init__(self):
+        super().__init__()
+
+    def inference(self, input_list, output_list, options=None):
+        img1 = Image.open(input_list[0]).convert('RGB')
+        img1 = ImageOps.grayscale(img1)
+        img1.save(output_list[0])
+
+
+
+class ExampleTask21(ITaskProcess):
     def __init__(self):
         super().__init__()
 
