d83e16d83c9fee80a277ae575f40d72f6c67e7d8
dt8 <dt8@game4219.com>
2021-09-08 15:42:47 +0800

    Some examples.

--- dg_ai_platform/task_process_1to1.py
+++ dg_ai_platform/task_process_1to1.py
@@ -6,6 +6,16 @@ class TaskProcess1to1(ITaskProcess):
         super().__init__()
 
     def inference(self, input_list, output_list, options=None):
+        #load input
         img1 = Image.open(input_list[0]).convert('RGB')
+
+        #use option param
+        if options is not None:
+            if 'scale' in options:
+                scale_v = options['scale']
+                img1 = img1.resize((int(img1.width * scale_v), int(img1.height * scale_v)))
+        #Process images
         img1 = ImageOps.grayscale(img1)
+
+        #save output
         img1.save(output_list[0])
